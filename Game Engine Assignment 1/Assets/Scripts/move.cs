95a5a2dab70dc054542d60cbe69b4de6d8d3a4aa
Matthew <matthew.mcpherson1@ontariotechu.net>
2021-09-25 17:35:41 -0400

    Project created
    added basic movement, enemy collision and following camera

---
+++ Game Engine Assignment 1/Assets/Scripts/move.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+    private float dirX, dirZ, dirY;
+    private bool grounded;
+    public Vector3 jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3f;
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, 5f, 0f);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * speed;
+        //dirZ = Input.GetAxis("Vertical") * speed;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jump, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(dirX, rb.velocity.y, rb.velocity.z);
+    }
+}
