18c7445d6bc5cd1bdd5f72bc77a770267534ce6d
Matthew <matthew.mcpherson1@ontariotechu.net>
2021-09-27 14:35:34 -0400

    Added Menu and Scene changing

---
+++ Game Engine Assignment 1/Assets/Scripts/Scene_Change.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Scene_Change : MonoBehaviour
+{
+    public void changeScene()
+    {
+        SceneManager.LoadScene("Level Prototype");
+    }
+
+}
